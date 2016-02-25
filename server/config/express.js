932b2fca6bad1fe3c554db2328deece9a7f86579
aee <a.etienne@galitt.com>
2016-02-25 13:46:02 +0100

    ajout headers pour requete application mobile

--- server/config/express.js
+++ server/config/express.js
@@ -49,6 +49,14 @@ export default function (app) {
     })
   }));
 
+  // Add response headers
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+    return next();
+  });
+
   /**
    * Lusca - express server security
    * https://github.com/krakenjs/lusca
