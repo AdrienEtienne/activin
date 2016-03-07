072b4996a328a9d70135700cfb6a7da3c26515e1
aee <a.etienne@galitt.com>
2016-03-07 12:11:23 +0100

    version dynamic

--- client/components/footer/footer.directive.js
+++ client/components/footer/footer.directive.js
@@ -1,12 +1,15 @@
 'use strict';
 
 angular.module('activinApp')
-  .directive('footer', function() {
-    return {
-      templateUrl: 'components/footer/footer.html',
-      restrict: 'E',
-      link: function(scope, element) {
-        element.addClass('footer');
-      }
-    };
-  });
+	.directive('footer', function () {
+		return {
+			templateUrl: 'components/footer/footer.html',
+			restrict: 'E',
+			link: function (scope, element) {
+				element.addClass('footer');
+			},
+			controller: function ($scope, appConfig) {
+				$scope.version = appConfig.version;
+			}
+		};
+	});
