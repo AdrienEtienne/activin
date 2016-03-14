db15ce8c52758c4628c3f9aeed1550de862aaef8
aee <a.etienne@galitt.com>
2016-03-14 17:00:02 +0100

    form for application upload

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,20 +1,37 @@
 'use strict';
 
-(function() {
+(function () {
 
-class AdminController {
-  constructor(User) {
-    // Use the User $resource to fetch all users
-    this.users = User.query();
-  }
+	class AdminController {
+		constructor(User, $http) {
+			this.http = $http;
 
-  delete(user) {
-    user.$remove();
-    this.users.splice(this.users.indexOf(user), 1);
-  }
-}
+			// Use the User $resource to fetch all users
+			this.users = User.query();
+			this.regexVersion = /^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}(\-(alpha|beta)\.[0-9]{1,3})?$/;
+		}
 
-angular.module('activinApp.admin')
-  .controller('AdminController', AdminController);
+		delete(user) {
+			user.$remove();
+			this.users.splice(this.users.indexOf(user), 1);
+		}
+
+		uploadApplication(form) {
+			var fd = new FormData();
+			//Take the first selected file
+			fd.append('file', form.file);
+
+			this.http.post('/api/applications', fd, {
+				withCredentials: true,
+				headers: {
+					'Content-Type': undefined
+				},
+				transformRequest: angular.identity
+			}).success(console.log('ok')).error(console.log('nok'));
+		}
+	}
+
+	angular.module('activinApp.admin')
+		.controller('AdminController', AdminController);
 
 })();
