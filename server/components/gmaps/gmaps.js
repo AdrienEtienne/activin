df7c99dc5eda1cb8915db250f2a4589c81026667
aee <a.etienne@galitt.com>
2016-03-22 17:36:41 +0100

    add model prediction

--- server/components/gmaps/gmaps.js
+++ server/components/gmaps/gmaps.js
@@ -3,54 +3,59 @@
 import appConfig from '../../config/environment';
 import GooglePlaces from 'googleplaces';
 import Promise from 'bluebird';
+import Prediction from './prediction';
 import _ from 'lodash';
 
-var googlePlaces = new GooglePlaces(appConfig.googleApi.key, 'json');
-
-function isConfigured() {
-	return appConfig.googleApi.key === 'id' ? false : true;
-}
-
-function catchKeyError() {
-	return new Promise(function (resolve, reject) {
-		reject(new Error('No key defined!'));
-	});
-}
+var googlePlaces = null;
+var currentKey = null;
 
 var gmaps = {
-	isConfigured: isConfigured,
-
-	getPredictions: function (input) {
-		if (!isConfigured())
-			return catchKeyError();
-		else
-			return new Promise(function (resolve, reject) {
-				var parameters = {
-					input: input || ''
-				};
-				googlePlaces.placeAutocomplete(parameters, function (err, response) {
-					if (err) {
-						reject(err);
-					} else {
-						resolve(response.predictions);
-					}
-				});
-			});
+	getGooglePlace: function () {
+		if (googlePlaces && currentKey === appConfig.googleApi.key) {
+			return googlePlaces;
+		} else {
+			currentKey = appConfig.googleApi.key;
+			googlePlaces = new GooglePlaces(appConfig.googleApi.key, 'json');
+			return googlePlaces;
+		}
 	},
 
-	getDetails: function (prediction) {
-		if (!isConfigured())
-			return catchKeyError();
-		else
-			return new Promise(function (resolve, reject) {
-				googlePlaces.placeDetailsRequest(prediction, function (err, response2) {
-					if (err) {
-						reject(err);
-					} else {
-						resolve(response.result);
-					}
-				})
+	getPredictions: function (input) {
+		return new Promise(function (resolve, reject) {
+			var parameters = {
+				input: input || ''
+			};
+			gmaps.getGooglePlace().placeAutocomplete(parameters, function (err, response) {
+				if (err) {
+					reject(err);
+				} else if (response.error_message) {
+					reject(new Error(response.error_message));
+				} else {
+					resolve(_.map(response.predictions, function (result) {
+						return new Prediction(result.place_id, result.description);
+					}));
+				}
 			});
+		});
+	},
+
+	getDetails: function (placeid) {
+		return new Promise(function (resolve, reject) {
+			gmaps.getGooglePlace().placeDetailsRequest({
+				placeid: placeid
+			}, function (err, response) {
+				if (err) {
+					reject(err);
+				} else if (response.error_message) {
+					reject(new Error(response.error_message));
+				} else {
+					resolve({
+						name: response.result.name,
+						location: response.result.geometry.location
+					});
+				}
+			})
+		});
 	}
 };
 
--- server/components/gmaps/gmaps.spec.js
+++ server/components/gmaps/gmaps.spec.js
@@ -2,50 +2,68 @@
 
 import config from '../../config/environment';
 import gmaps from './gmaps';
+import Prediction from './prediction';
 
 describe('Components: Gmaps', function () {
-
-	describe('isConfigured()', function () {
-		var key;
-		before(function () {
-			key = config.googleApi.key;
-		})
-		after(function () {
-			config.googleApi.key = key;
-		})
-
-		it('should not be configured if equal id', function () {
-			config.googleApi.key = 'id';
-			gmaps.isConfigured().should.equal(false);
-		});
-
-		it('should be configured', function () {
-			config.googleApi.key = 'true key';
-			gmaps.isConfigured().should.equal(true);
-		});
+	var key;
+	before(function () {
+		key = config.googleApi.key;
 	});
 
-	describe('getPredictions()', function () {
-		var key;
-		before(function () {
-			key = config.googleApi.key;
-		})
-		after(function () {
-			config.googleApi.key = key;
-		})
+	after(function () {
+		config.googleApi.key = key;
+	});
+
+	describe('getPredictions(input)', function () {
+		it('should return an error if no API key', function (done) {
+			config.googleApi.key = 'id';
+			gmaps.getPredictions().catch(function (err) {
+				should.exist(err);
+				done();
+			});
+		});
 
 		it('should return an array of predictions', function (done) {
-			config.googleApi.key = 'true key';
+			config.googleApi.key = key;
 			gmaps.getPredictions().then(function (predictions) {
 				predictions.should.be.instanceOf(Array);
 				done();
 			});
 		});
 
+		it('should return an array of predictions', function (done) {
+			config.googleApi.key = key;
+			gmaps.getPredictions().then(function (predictions) {
+				predictions.should.have.length(5);
+				for (var i = predictions.length - 1; i >= 0; i--) {
+					predictions[i].should.be.instanceOf(Prediction);
+				}
+				done();
+			});
+		});
+	});
+
+	describe('getDetails(prediction)', function () {
+		var prediction;
+		before(function (done) {
+			gmaps.getPredictions().then(function (predictions) {
+				prediction = predictions[0];
+				done();
+			});
+		});
+
 		it('should return an error if no API key', function (done) {
 			config.googleApi.key = 'id';
-			gmaps.getPredictions().catch(function (err) {
+			gmaps.getDetails(prediction.getId()).catch(function (err) {
 				should.exist(err);
+				config.googleApi.key = key;
+				done();
+			});
+		});
+
+		it('should return place details', function (done) {
+			gmaps.getDetails(prediction.getId()).then(function (detail) {
+				should.exist(detail.location);
 				done();
 			});
 		});
